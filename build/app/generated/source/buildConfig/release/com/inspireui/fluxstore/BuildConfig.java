38bf8c6840821d935764697201a35c13802ac1b1
vois-asakry <mo.askary@gmail.com>
2023-07-16 02:36:46 +0300

    first

---
+++ build/app/generated/source/buildConfig/release/com/inspireui/fluxstore/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.inspireui.fluxstore;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.souqocta.www";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 62359;
+  public static final String VERSION_NAME = "27.0.26";
+}
